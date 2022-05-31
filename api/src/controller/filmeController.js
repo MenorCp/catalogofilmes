7a05de66f0027a6febb9e5c060f76f4d39248c1b
joao victor andrade <jvas2005@outlook.com>
2022-05-31 09:03:46 -0300

    FINISH THIS SHIT BRO

--- api/src/controller/filmeController.js
+++ api/src/controller/filmeController.js
@@ -1,4 +1,4 @@
-import { alterarImagem, inserirFilme } from "../repository/filmeRepository.js";
+import { alterarImagem, inserirFilme, listarTodosFilmes, buscarPorId, buscarPorNome, removerFilme, alterarFilme } from "../repository/filmeRepository.js";
 
 import multer from 'multer'
 
@@ -53,5 +53,105 @@ server.put('/filme/:id/capa', upload.single('capa') , async (req, resp) => {
         })
     }
 })
+
+server.get('/filme', async(req, resp) => {
+    try{
+        const resposta = await listarTodosFilmes();
+
+        resp.send(resposta);
+    }catch(err){
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+server.get('/filme/busca', async(req, resp) => {
+    try{
+        const { Nome } = req.query;
+        const resposta = await buscarPorNome(Nome);
+
+        if (!resposta || resposta.length == 0)
+                resp.status(404).send([])
+                
+        else
+            resp.send(resposta);
+            
+        }catch(err){
+            resp.status(400).send({
+                erro: err.message
+            })
+    }
+})
+
+
+server.get('/filme/:id', async(req, resp) => {
+    try{
+        const { id } = req.params;
+        const resposta = await buscarPorId(id);
+
+            resp.send(resposta);
+        }catch(err){
+            resp.status(400).send({
+                erro: err.message
+            })
+    }
+})
+
+server.delete('/filme/:id', async (req, resp) => {
+    try{
+        const { id } = req.params;
+
+        const resposta = await removerFilme(id);
+
+        if(resposta != 1)
+            throw new Error ('O filme não pode ser removido.....');
+
+        resp.status(204).send();
+
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message
+        })
+    }
+})
+
+
+server.put('/filme/:id', async (req, resp) => {
+    try{
+        const { id } = req.params;
+        const filme = req.body;
+
+        if(!filme.nome){
+            throw new Error('Nome do filme e obrigatório !!')
+        }
+        if(!filme.sinopse){
+            throw new Error('A sinopse do filme e obrigatória !!')
+        }
+        if(filme.avaliacao == undefined || filme.avaliacao < 0 ){
+            throw new Error('A avaliação do filme e obrigatória !!')
+        }
+        if(filme.disponivel == undefined){
+            throw new Error('É obrigatorio informar se o filme esta disponivel !!')
+        }
+        if(!filme.usuario){
+            throw new Error('Usuario não logado !!')
+        }
+
+
+        const resposta = await alterarFilme(id, filme);
+        if(resposta != 1)
+             throw new Error ('O filme não pode ser alterado..')
+
+        else
+            resp.status(204).send();
+    }catch(err){
+        resp.status(404).send({
+            erro: err.message 
+        })
+    }
+})
+
+
 
 export default server;
